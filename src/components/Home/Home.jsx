a9576d519f3abd0d53d530c3c8baee934fa9f207
Ishan <ishan.kulkarni@somaiya.edu>
2021-11-04 00:14:54 +0530

    Side bar converted to dropdown

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import HomeNavbar from './HomeNavbar'
 import { Switch, Route } from 'react-router-dom'
 import NFA_to_DFA from './Converters/NFA-DFA/NFA_to_DFA'
 import CFG_to_CNF from './Converters/CFG-CNF/CFG_to_CNF'
@@ -7,15 +6,12 @@ import CFG_to_CNF from './Converters/CFG-CNF/CFG_to_CNF'
 function Home() {
     return (
         <div>
-            <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased">
+            <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased">
                 <div className="grid grid-cols-12">
-                    <div className="col-span-5 sm:col-span-4 md:col-span-2">
-                        <HomeNavbar />
-                    </div>
-                    <div className="col-span-7 sm:col-span-8 md:col-span-10  overscroll-auto  dark:bg-gray-700 w-100">
+                    <div className="col-span-12 sm:col-span-8 md:col-span-10  overscroll-auto  dark:bg-gray-700 w-100">
                         <Switch>
                             <Route path='/dashboard' exact component={NFA_to_DFA} />
-                            <Route path='/dashboard/CNF-to-CNF' component={CFG_to_CNF} />
+                            <Route path='/dashboard/CFG-to-CNF' component={CFG_to_CNF} />
                         </Switch>
                     </div>
 
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from 'react';
-import { Disclosure } from '@headlessui/react'
+import { useEffect, useState, Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import logo from '../../utils/Images/logo.png'
-const navigation = [
-    { name: 'Dashboard', href: '/dashboard' },
-    { name: 'About Us', href: '/about-us' },
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid'
+
+
+
+const converters = [
+    { name: 'NFA TO DFA CONVERTER', href: "/dashboard" },
+    { name: 'CFG TO CNF CONVERTER', href: "/dashboard/CFG-to-CNF" },
 ]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
+
 
 export default function Navbar() {
     const [darkMode, setDarkMode] = useState(false);
@@ -23,6 +28,9 @@ export default function Navbar() {
             setDarkMode(false);
         }
     }, [])
+
+    const Location = useLocation();
+
 
     useEffect(() => {
         if (darkMode) {
@@ -63,19 +71,55 @@ export default function Navbar() {
                                 </div>
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
-                                        {navigation.map((item) => (
-                                            <NavLink
-                                                key={item.name}
-                                                to={item.href}
-                                                className={classNames(
-                                                    'text-gray-300 hover:bg-gray-700 hover:text-white no-underline px-3 py-2 rounded-md text-sm font-medium'
-                                                )}
-                                                activeClassName='bg-gray-900 text-white hover:bg-gray-900 hover:text-white'
+                                        <Menu as="div" className="relative inline-block text-left">
+                                            <div>
+                                                <Menu.Button className={`inline-flex justify-center w-full px-4 py-2 text-sm font-medium rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-gray-300 hover:bg-gray-700 hover:text-white no-underline px-3 py-2 rounded-md text-sm font-medium  ${Location.pathname.startsWith('/dashboard') ? 'bg-gray-900 text-white hover:bg-gray-900 hover:text-white' : ''}`}>
+                                                    Home
+                                                    <ChevronDownIcon
+                                                        className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                                                        aria-hidden="true"
+                                                    />
+                                                </Menu.Button>
+                                            </div>
+                                            <Transition
+                                                as={Fragment}
+                                                enter="transition ease-out duration-100"
+                                                enterFrom="transform opacity-0 scale-95"
+                                                enterTo="transform opacity-100 scale-100"
+                                                leave="transition ease-in duration-75"
+                                                leaveFrom="transform opacity-100 scale-100"
+                                                leaveTo="transform opacity-0 scale-95"
                                             >
-                                                {item.name}
-                                            </NavLink>
+                                                <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                    <div className="px-1 py-1 ">
+                                                        {
+                                                            converters.map((value, key) =>
+                                                                <Menu.Item key={key}>
+                                                                    <NavLink
+                                                                        className={'my-2 group flex rounded-md items-center w-full px-2 py-2 text-sm no-underline px-3 py-2 rounded-md text-sm font-medium text-gray-900 bg-gray-300 shadow-inner hover:text-gray-700 hover:bg-gray-500 dark:text-gray-500 dark:bg-gray-900 dark:hover:text-gray-300 dark:hover:bg-gray-700'}
+                                                                        to={value.href}
+                                                                        exact
+                                                                        key={key}
+                                                                        activeClassName='text-white bg-red-600 hover:text-gray-100 hover:bg-red-600 dark:text-white dark:bg-red-600 dark:hover:text-gray-100 dark:hover:bg-red-600'
+                                                                    >
+                                                                        {value.name}
+                                                                    </NavLink>
+                                                                </Menu.Item>)
+                                                        }
 
-                                        ))}
+                                                    </div>
+                                                </Menu.Items>
+                                            </Transition>
+                                        </Menu>
+                                        <NavLink
+                                            to={'/about-us'}
+                                            className={classNames(
+                                                'text-gray-300 hover:bg-gray-700 hover:text-white no-underline px-3 py-2 rounded-md text-sm font-medium'
+                                            )}
+                                            activeClassName='bg-gray-900 text-white hover:bg-gray-900 hover:text-white'
+                                        >
+                                            About Us
+                                        </NavLink>
                                     </div>
                                 </div>
                             </div>
@@ -107,20 +151,45 @@ export default function Navbar() {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-1">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as={NavLink}
-                                    exact
-                                    to={item.href}
-                                    className={classNames(
-                                        'text-gray-300 hover:bg-gray-700 hover:text-white no-underline block px-3 py-2 rounded-md text-base font-medium'
-                                    )}
-                                    activeClassName='bg-gray-900 text-white hover:bg-gray-900 hover:text-white'
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
+                            <Disclosure >
+                                {({ open }) => (
+                                    <>
+
+                                        <Disclosure.Button className={`flex justify-between w-100 text-left text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline block px-3 py-2 rounded-md text-base font-medium ${Location.pathname.startsWith('/dashboard') ? 'bg-gray-900 text-white hover:bg-gray-900 hover:text-white' : ''}`}>
+                                            <span>Home</span><ChevronUpIcon
+                                                className={`${open ? 'transform rotate-180' : ''
+                                                    } w-5 h-5 text-purple-500`}
+                                            />
+                                        </Disclosure.Button>
+                                        <Disclosure.Panel className="text-gray-500">
+                                            {converters.map((value, key) =>
+                                                <Disclosure.Button
+                                                    key={key}
+                                                    as={NavLink}
+                                                    className={'my-2 group flex rounded-md items-center w-full px-2 py-2 text-sm no-underline px-3 py-2 rounded-md text-sm font-medium text-gray-900 bg-gray-300 shadow-inner hover:text-gray-700 hover:bg-gray-500 dark:text-gray-500 dark:bg-gray-900 dark:hover:text-gray-300 dark:hover:bg-gray-700'}
+                                                    to={value.href}
+                                                    exact
+                                                    activeClassName='text-white bg-red-600 hover:text-gray-100 hover:bg-red-600 dark:text-white dark:bg-red-600 dark:hover:text-gray-100 dark:hover:bg-red-600'
+                                                >
+                                                    {value.name}
+                                                </Disclosure.Button>
+                                            )
+                                            }
+                                        </Disclosure.Panel>
+                                    </>
+                                )}
+                            </Disclosure>
+                            <Disclosure.Button
+                                as={NavLink}
+                                exact
+                                to={'/about-us'}
+                                className={classNames(
+                                    'text-gray-300 hover:bg-gray-700 hover:text-white no-underline block px-3 py-2 rounded-md text-base font-medium'
+                                )}
+                                activeClassName='bg-gray-900 text-white hover:bg-gray-900 hover:text-white'
+                            >
+                                {'About Us'}
+                            </Disclosure.Button>
                         </div>
                     </Disclosure.Panel>
                 </>
