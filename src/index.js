84248a6ab7c5ebacf4cac63eab56af7c7fe103fc
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2022-03-31 01:39:39 +0530

    PWA Done :tada:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -17,7 +16,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-serviceWorkerRegistration.register();
 
 reportWebVitals();
