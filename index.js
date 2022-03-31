5657b949337293c6b025e86cf631697c7b9413d1
Ishan <ishan.kulkarni@somaiya.edu>
2022-03-31 15:26:31 +0530

    FIX: index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 const path = require("path");
 
-const port = 4000
+const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, "build")));
 console.log(path.join(__dirname, "build"))
