ab32c47931d06ce7f6d88c0843ee87aedf5ddf60
Ishan <ishan.kulkarni@somaiya.edu>
2022-03-31 15:32:40 +0530

    FIX: index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const path = require("path");
 const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, "build")));
-console.log(path.join(__dirname, "build"))
+app.use('/*',express.static(path.join(__dirname, "build")))
+
+
 
 app.listen(port,()=>{
     console.log(`frontend server started on port ${port}`)
